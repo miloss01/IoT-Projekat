a8fa85a59c32827b1d4c63f12dea968fd1f657aa
miloss01 <mikicamiki.bat@gmail.com>
2023-12-12 19:36:40 +0100

    Comment callings in main

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     # run_dht(rdht1_settings, threads, stop_event)
     # run_dht(rdht2_settings, threads, stop_event)
     
-    run_uds(dus1_settings, threads, stop_event)
+    # run_uds(dus1_settings, threads, stop_event)
 
     # run_pir(dpir1_settings, threads, stop_event, dpir1_event)
     # run_pir(rpir1_settings, threads, stop_event, rpir1_event)
